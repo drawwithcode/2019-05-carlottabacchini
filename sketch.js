b2a3b0e88dd1809a31262edb1e68127dc52c8d9c
Carlotta Bacchini <carlotta@MacBook-Pro-di-Carlotta-2.local>
2020-01-15 12:27:27 +0100

    05

--- sketch.js
+++ sketch.js
@@ -4,7 +4,16 @@ var r;
 var g;
 var b;
 var intro = true;
+var shape;
+let cube;
+let instructions;
+let voice;
 
+function preload() {
+cube = loadImage('assets/cube.png');
+instructions = loadImage('assets/instructions.png')
+
+}
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
     graphics = createGraphics(300, 300);
@@ -15,15 +24,27 @@ function setup() {
   r = Math.round(random(255));
   g = Math.round(random(255));
   b = Math.round(random(255));
+
+voice = new p5.Speech();
+
+  voice.start;
+  voice.setPitch(0.8);
+  voice.setRate(0.8)
+
+
 }
+
+
 
 function draw() {
 
   if (intro) {
-    background('red')
-    
+    background(16,15,34)
+image(cube, -300, -500, 600, 600)
+image(instructions, -500, 180, 1000*1.2, 200*1.2)
+document.getElementById("instructions").style.visibility = "hidden";
+document.getElementById("param").style.visibility = "hidden";
 
-    //metti qua le cose per la schermata iniziale
 
   }
 
@@ -44,7 +65,7 @@ function draw() {
   texture(graphics);
   rotateY(0);
   rotateX(0)
-  box(150);
+  shape = box(150);
   translate(300, 0, 0);
 
   if (keyIsDown(82)) {
@@ -54,12 +75,16 @@ function draw() {
         r = 255;
       }
       r = Math.round(r)
+      voice.speak('red increases');
+      voice.stop();
     } else if (keyIsDown(DOWN_ARROW)) {
         r -= 5;
         if (r <= 0) {
           r = 0
         }
         r = Math.round(r)
+        voice.speak('red decreases');
+        voice.stop();
       }
   console.log('r ',r)
 
@@ -72,12 +97,16 @@ function draw() {
         g = 255;
       }
       g = Math.round(g)
+      voice.speak('green increases');
+      voice.stop();
     } else if (keyIsDown(DOWN_ARROW)) {
         g -= 5;
         if (g <= 0) {
           g = 0
         }
         g = Math.round(g)
+        voice.speak('green decreases');
+        voice.stop();
       }
   console.log('g ',g)
   }
@@ -89,12 +118,16 @@ function draw() {
         b = 255;
       }
       b = Math.round(b)
+      voice.speak('blue increases');
+      voice.stop();
     } else if (keyIsDown(DOWN_ARROW)) {
       b -= 5;
         if (b <= 0) {
           b = 0
         }
       b = Math.round(b)
+      voice.speak('blue decrease');
+      voice.stop();
       }
   console.log('b ',b)
   }
@@ -105,5 +138,9 @@ function draw() {
 function keyPressed() {
   if (keyCode === ENTER) {
     intro = false
+      voice.setVoice('Cellos')
+      voice.speak("Welcome to the cube webcam");
+    document.getElementById("instructions").style.visibility = "visible";
+    document.getElementById("param").style.visibility = "visible";
 }
 }
